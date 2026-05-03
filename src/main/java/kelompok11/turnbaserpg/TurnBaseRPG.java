e6b64dfd9a71eaa9e5b70a17d84401a911ab9df2
Nico <nicolausnl05@gmail.com>
2026-05-03 16:59:29 +0700

    first commit

---
+++ src/main/java/kelompok11/turnbaserpg/TurnBaseRPG.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package kelompok11.turnbaserpg;
+
+/**
+ *
+ * @author Pongo
+ */
+public class TurnBaseRPG {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
